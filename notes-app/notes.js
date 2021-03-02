813a0fc2e4e609e994bb63eff75f22046c42fbf8
Athul Das <meathuldas010@gmail.com>
2021-03-02 11:47:04 +0530

    Node Course

---
+++ notes-app/notes.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const getNotes = function () {
+    return 'Your notes...'
+}
+const addNotes = function(title,body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+    
+    if(duplicateNotes.length == 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes)
+        console.log('New note added!')
+    }
+    else {
+        console.log('Note title taken!')   
+    }
+
+    
+}
+
+const removeNotes = function(title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function(note){
+        return note.title !== title
+    })
+    if(notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note Removed'));
+        saveNotes(notesToKeep)
+    }else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+
+
+}
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch {
+        return []
+    }
+}
+module.exports =  {
+    getNotes: getNotes,
+    addNotes: addNotes,
+    removeNotes:removeNotes
+}
+
+
+
